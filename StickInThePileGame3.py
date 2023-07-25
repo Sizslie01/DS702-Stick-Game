62d84711dbd1f3ca27b51611ec7d0e1bf17dd66e
Sizslie01 <94440933+Sizslie01@users.noreply.github.com>
2023-07-26 01:15:12 +0700

    Define bot take stick to function
    Create the function that calculate how many bot take the stick and update the condition of user function.

--- StickInThePileGame3.py
+++ StickInThePileGame3.py
@@ -1,54 +1,58 @@
 #660631097 Tanasin Sriwichai
-#HW1D_stickpile
-#12-7-2023
+#HW1E_stickpile
+#26-7-2023
+import random
 
 name = input('What is your name :')
-n_stick = 20
-print('There are {} sticks in the pile'.format(n_stick))
+n_sticks = int(input('How many sticks (N) in the pile:'))
+print('There are {} sticks in the pile'.format(n_sticks))
 
-def user_take_stick (n_of_sticks):
+def user_take_stick (n_of_sticks, take_not_more_than = 2, winner_condition = 1):
     n_take = int(input('How many you will take (1 or 2):'))
-    if n_take > 2: #1st condition check the limit number of taken stick and amount of stick left.
+    if n_take > take_not_more_than: #1st condition check the limit number of taken stick and amount of stick left.
         print('No you cannot take more than 2 sticks!\n')
-    elif n_take < 1:
+    elif n_take < winner_condition:
         print('No you cannot take less than 1 stick!\n')
     elif n_take > n_of_sticks:
         print('There are no enough sticks to take.\n')
     else:
         n_of_sticks -= n_take
-        if n_of_sticks == 1:#2nd condition check how many stick left after taken out.
+        if n_of_sticks == winner_condition:#2nd condition check how many stick left after taken out.
             print('There are',n_of_sticks,'stick in the pile.\n')
         elif n_of_sticks > 0: 
             print('There are',n_of_sticks,'sticks in the pile.\n')
         indicator = 'user'    
         return indicator ,n_of_sticks
     
-        
+def bot_take_stick (n_of_sticks, take_not_more_than = 2, winner_condition = 1):
+    
+    taken_pattern = winner_condition + take_not_more_than
+    stick_left = ((n_of_sticks // taken_pattern)*taken_pattern) + winner_condition
+    
+    if stick_left == n_of_sticks:
+        if n_of_sticks == winner_condition:
+            n_bot_take = winner_condition
+        else:
+            n_bot_take = random.randint(winner_condition,take_not_more_than)
+    elif stick_left < n_of_sticks:
+        n_bot_take = n_of_sticks - stick_left
+    else:
+        n_bot_take = n_of_sticks - stick_left + taken_pattern
+    n_of_sticks -= n_bot_take    
+    print('I take {} stick, there are {} sticks in the pile.'.format(n_bot_take,n_of_sticks))
+    return n_of_sticks    
+    
 def winner_indicator (indicator,name):
     if indicator == 'user' :
         print('You take the last stick,\n I WON (Python WON)')
     else:
         print('I take the last stick,\n YOU WON ({} WON)'.format(name))
+        
 
 
 
 
+    
+    
 
-while n_stick > 0: #create while loop until number of stick was out
-    remainder = n_stick 
-    while n_stick == remainder: #check that the stick was taken out by remainder.
-        n_out = int(input('{}, how many sticks you will take (1 or 2):'.format(name)))
-        n_stick = take_stick(n_out,n_stick)
-        if n_stick == 0: #save the value of indicator for finding winner 0 for user and 1 for computer.
-            indicator = 0
-    if n_stick >= 1:
-        if n_stick > 1:
-            remainder = n_stick
-            while n_stick == remainder:#check that the stick was taken out by remainder.
-                n_bot_out = random.randint(1,2)
-                print('I, smart computer, takes :',n_bot_out)
-                n_stick = take_stick(n_bot_out,n_stick)
-        elif n_stick == 1: #save the value of indicator for finding winner 0 for user and 1 for computer.
-            indicator = 1
-            break
-                
+
